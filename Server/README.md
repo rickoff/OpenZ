0fbe1221aa57dfa387c7323fcee77df7c650bc4b
Rickoff <38435435+rickoff@users.noreply.github.com>
2026-08-27 02:05:08 +0200

    Delete Server directory

--- Server/README.md
+++
@@ -1,5 +0,0 @@
-# CoreScripts
-
-These are the scripts used to implement most of the essential server logic in TES3MP, including gameplay adjustments for multiplayer as well as state saving and loading.
-
-* TES3MP version: 0.8.1
--- Server/scripts/customScripts.lua
+++
@@ -1,7 +0,0 @@
--- Load up your custom scripts here! Ideally, your custom scripts will be placed in the scripts/custom folder and then get loaded like this:
---
--- require("custom/yourScript")
---
--- Refer to the Tutorial.md file for information on how to use various event and command hooks in your scripts.
-
-OpenZ_CarScript = require("custom.OpenZ_CarScript")
